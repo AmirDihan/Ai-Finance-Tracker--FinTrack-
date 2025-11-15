828d8ae7eee39567961f0e86b0c06842ae919a59
AmirDihan <amirhossan160@gmail.com>
2025-11-16 00:30:48 +0600

    Initial frontend setup and header component completed with authentication

---
+++ components/header.jsx
@@ -0,0 +1,65 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { LayoutDashboard, PenBox } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4  flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="FinTrack Logo"
+            height={80}
+            width={200}
+            className="h-20 w-auto object-contain"
+          ></Image>
+        </Link>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href="/dashboard"
+              className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+            >
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+            <Link href="/transaction" className="flex items-center gap-2">
+              <Button>
+                <PenBox size={18} />
+                <span className="hidden md:inline">Add Transaction</span>
+              </Button>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-14 h-14",
+                  avatarImage: "w-14 h-14",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
